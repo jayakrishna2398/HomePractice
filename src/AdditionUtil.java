cfbb7a9e3f29ab7331a11fae60a8e75d576dd7b9
Training <jayakrishnakrish1998@gmail.com>
2019-08-14 23:18:04 +0530

    Add pom.xml and src

---
+++ src/AdditionUtil.java
@@ -0,0 +1,10 @@
+package com.revature.Calculator;
+
+import com.revature.Calculator_app.Addition4;
+
+public class AdditionUtil {
+	public static void main(String[] args) {
+		int b = Addition4.addReturn(5,6);
+		System.out.println(b);
+	}
+}
--- src/Naming.java
+++ src/Naming.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 		System.out.println("Invalid name");
 	}
 	else {
-		System.out.println("valid name");
+		System.out.println("jk");
 	}
 }
 }
