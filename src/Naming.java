2c510ad6bd4d54983ed3d727a3cbd182f157936e
Training <jayakrishnakrish1998@gmail.com>
2019-08-14 22:32:49 +0530

    Add pom.xml and src

---
+++ src/Naming.java
@@ -0,0 +1,12 @@
+
+public class Naming {
+public static void main(String[] args) {
+	String str = "jayakrishna";
+	if((str == " ") || (str.trim()== null)) {
+		System.out.println("Invalid name");
+	}
+	else {
+		System.out.println("valid name");
+	}
+}
+}
